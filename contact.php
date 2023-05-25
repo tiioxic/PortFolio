ac0bd8cf6fcf538804baf512201d20b00fc19ae4
tiioxic <toto27160@hotmail.fr>
2023-05-25 12:16:36 +0200

    update

--- contact.php
+++
@@ -1,9 +0,0 @@
-        <!-- header -->
-<?php 
-$tittle = 'Contact';
-include('partials/_header.php') ?>
-
-<?php include('partials/contact/_contact.php') ?>
-
-        <!-- include footer -->
-<?php include('partials/_footer.php') ?>
