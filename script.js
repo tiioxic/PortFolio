f1aa9b28620ca23b978db6a7b0512156a3df7d39
tiioxic <toto27160@hotmail.fr>
2023-06-01 21:02:36 +0200

    update

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const hamburgerButton = document.querySelector(".nav-toggler");
 const navigation = document.querySelector(".items-nav");
 const navbar = document.querySelector(".navbar");
+const logo = document.querySelector(".logo");
 
 hamburgerButton.addEventListener("click", hamburger);
 
@@ -8,4 +9,5 @@ function hamburger() {
   hamburgerButton.classList.toggle("active");
   navigation.classList.toggle("active");
   navbar.classList.toggle("active");
+  logo.classList.toggle("active");
 }
