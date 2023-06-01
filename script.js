36b0953437688eb10dd3c6d1fde967ca45474aff
tiioxic <toto27160@hotmail.fr>
2023-06-01 20:18:12 +0200

    udpate

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const hamburgerButton = document.querySelector(".nav-toggler");
-const navigation = document.querySelector("nav");
+const navigation = document.querySelector(".items-nav");
+const navbar = document.querySelector(".navbar");
 
 hamburgerButton.addEventListener("click", hamburger);
 
 function hamburger() {
   hamburgerButton.classList.toggle("active");
   navigation.classList.toggle("active");
+  navbar.classList.toggle("active");
 }
