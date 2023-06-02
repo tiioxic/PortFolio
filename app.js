f09e87f478d6791535ceda0b9a738eb9b6633f14
tiioxic <toto27160@hotmail.fr>
2023-06-02 15:55:08 +0200

    update

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const btn = document.querySelector(".btn-contact");
 const TL1 = new TimelineMax({ paused: true });
 
 TL1
-.from(tittle, 1, { x: -100, opacity: 0 });
+.from(tittle, 3, { x: -100, opacity: 0 });
 TL1
-.from(txt, 1, {opacity: 0 }, '-=0.4');
+.from(txt, 4, {opacity: 0 }, '-=0.4');
 TL1
-.from(btn, 1, {opacity: 0 }, '-=0.5');
+.from(btn, 5, {opacity: 0 }, '-=0.5');
 
 
 TL1.play();
